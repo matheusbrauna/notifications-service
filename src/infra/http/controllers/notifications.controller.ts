9d57ddeecb5c37f8335107ed38fc16ebbe7cb9eb
matheusbrauna <mattsilvahenrique102@gmail.com>
2022-12-17 19:53:46 -0300

    feat: create prisma repository

---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { SendNotificationUseCase } from 'src/application/use-cases/send-notification-use-case';
+
+import { CreateNotificationInput } from '../dtos/create-notification-input';
+
+@Controller('Notifications')
+export class NotificationsController {
+  constructor(private sendNotificationUseCase: SendNotificationUseCase) {}
+
+  @Post()
+  async create(@Body() body: CreateNotificationInput) {
+    const { category, content, recipientId } = body;
+
+    const { notification } = await this.sendNotificationUseCase.execute({
+      category,
+      content,
+      recipientId,
+    });
+
+    return {
+      notification,
+    };
+  }
+}
