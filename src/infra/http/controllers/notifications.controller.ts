c3c05c8061a846793a52845dd7366f6f7d65be1d
matheusbrauna <mattsilvahenrique102@gmail.com>
2022-12-18 18:24:51 -0300

    feat: add notifications missing routes

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,67 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { SendNotificationUseCase } from '@application/use-cases/send-notification-use-case';
 import { CreateNotificationInput } from '../dtos/create-notification-input';
 import { NotificationViewModel } from '../view-models/notification-view-model';
+import { CancelNotificationUseCase } from '@application/use-cases/cancel-notification-use-case';
+import { ReadNotificationUseCase } from '@application/use-cases/read-notification-use-case';
+import { UnreadNotificationUseCase } from '@application/use-cases/unread-notification-use-case';
+import { GetRecipientNotificationsUseCase } from '@application/use-cases/get-recipient-notifications-use-case';
+import { CountRecipientNotificationsUseCase } from '@application/use-cases/count-recipient-notifications-use-case';
 
 @Controller('Notifications')
 export class NotificationsController {
-  constructor(private sendNotificationUseCase: SendNotificationUseCase) {}
+  constructor(
+    private sendNotificationUseCase: SendNotificationUseCase,
+    private cancelNotificationUseCase: CancelNotificationUseCase,
+    private readNotificationUseCase: ReadNotificationUseCase,
+    private unreadNotificationUseCase: UnreadNotificationUseCase,
+    private getRecipientNotificationsUseCase: GetRecipientNotificationsUseCase,
+    private countRecipientNotificationsUseCase: CountRecipientNotificationsUseCase,
+  ) {}
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotificationUseCase.execute({
+      notificationId: id,
+    });
+  }
+
+  @Get('count/from/:recipientId')
+  async countFromRecipient(@Param('recipientId') recipientId: string) {
+    const { count } = await this.countRecipientNotificationsUseCase.execute({
+      recipientId,
+    });
+
+    return {
+      count,
+    };
+  }
+
+  @Get('from/:recipientId')
+  async getFromRecipient(@Param('recipientId') recipientId: string) {
+    const { notifications } =
+      await this.getRecipientNotificationsUseCase.execute({
+        recipientId,
+      });
+
+    return {
+      notifications: notifications.map(NotificationViewModel.toHTTP),
+    };
+  }
+
+  @Patch(':id/read')
+  async read(@Param('id') id: string) {
+    await this.readNotificationUseCase.execute({
+      notificationId: id,
+    });
+  }
+
+  @Patch(':id/unread')
+  async unread(@Param('id') id: string) {
+    await this.unreadNotificationUseCase.execute({
+      notificationId: id,
+    });
+  }
 
   @Post()
   async create(@Body() body: CreateNotificationInput) {
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { SendNotificationUseCase } from '@application/use-cases/send-notification-use-case';
 import { DatabaseModule } from '@infra/database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
+import { CancelNotificationUseCase } from '@application/use-cases/cancel-notification-use-case';
+import { CountRecipientNotificationsUseCase } from '@application/use-cases/count-recipient-notifications-use-case';
+import { GetRecipientNotificationsUseCase } from '@application/use-cases/get-recipient-notifications-use-case';
+import { ReadNotificationUseCase } from '@application/use-cases/read-notification-use-case';
+import { UnreadNotificationUseCase } from '@application/use-cases/unread-notification-use-case';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotificationUseCase],
+  providers: [
+    SendNotificationUseCase,
+    CancelNotificationUseCase,
+    CountRecipientNotificationsUseCase,
+    GetRecipientNotificationsUseCase,
+    ReadNotificationUseCase,
+    UnreadNotificationUseCase,
+  ],
 })
 export class HttpModule {}
